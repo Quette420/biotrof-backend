ebdab47329d5468ba542f1647b3415b253f68713
George <gsaranov@gmail.com>
2023-06-12 14:49:40 +0300

    added IncorrectPasswordException handling

--- src/main/java/ru/volgau/graduatework/biotrofbackend/controllers/exception/RestResponseEntityExceptionHandler.java
+++ src/main/java/ru/volgau/graduatework/biotrofbackend/controllers/exception/RestResponseEntityExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import ru.volgau.graduatework.biotrofbackend.exceptions.IncorrectPasswordException;
 import ru.volgau.graduatework.biotrofbackend.exceptions.UserAlreadyExistsException;
 
 
@@ -20,6 +21,13 @@ public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionH
     @ExceptionHandler({UserAlreadyExistsException.class})
     public final ResponseEntity<Object> handleUserAlreadyExistsException(final RuntimeException e, ServletWebRequest request) {
         log.error("UserAlreadyExists ", e);
+        final ApiError apiError = message(e, request);
+        return handleExceptionInternal(e, apiError, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
+    }
+
+    @ExceptionHandler({IncorrectPasswordException.class})
+    public final ResponseEntity<Object> handleIncorrectPasswordException(final RuntimeException e, ServletWebRequest request) {
+        log.error("IncorrectPasswordException ", e);
         final ApiError apiError = message(e, request);
         return handleExceptionInternal(e, apiError, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
     }
